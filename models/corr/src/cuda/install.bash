1aa0638825ef14946c5ddd77ce075119296a3610
chenchr <531585839@qq.com>
2018-04-24 14:01:26 +0000

    change cuda sm

--- models/corr/src/cuda/install.bash
+++ models/corr/src/cuda/install.bash
@@ -1,2 +1,2 @@
 TORCH=$(python -c "import os; import torch; print(os.path.dirname(torch.__file__))")
-nvcc -c -o corr.cu.o corr_kernel.cu --gpu-architecture=compute_50 --gpu-code=compute_50 --compiler-options -fPIC -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
+nvcc -c -o corr.cu.o corr_kernel.cu --gpu-architecture=compute_61 --gpu-code=compute_61 --compiler-options -fPIC -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
